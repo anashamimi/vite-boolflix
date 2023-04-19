2cc59fc1042850206d6217e76a3c0769857eb253
anashamimi <anashamimi2@gmail.com>
2023-04-19 18:09:17 +0200

    add navbar

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,14 @@
 <template>
     <header>
-        
+        <nav class="navbar">
+            <div class="container-fluid">
+                <h2>Boolflix</h2>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Cerca un film">
+                    <button type="button" class="btn btn-outline-light">Cerca</button>
+                </form>
+            </div>
+        </nav>
     </header>
 </template>
 
@@ -17,5 +25,8 @@ import { store } from '../data/store';
 </script>
 
 <style lang="scss" scoped>
-
+    nav{
+        background-color: rgba(0,0,0,0.5);
+        color: red;
+    }
 </style>
