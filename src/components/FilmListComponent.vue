7bcd54c242a49b324308eb6ed01ff6fa6690f99a
anashamimi <anashamimi2@gmail.com>
2023-04-21 15:41:37 +0200

    add flags

--- src/components/FilmListComponent.vue
+++ src/components/FilmListComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-12 col-sm-3 col-md-2">
-    <vue-flip active-click height="300px">
+    <vue-flip active-click height="300px" class="vue-flip">
       <template v-slot:front>
         <div class="card-img">
           <img :src="image" :alt="title" class="img-fluid">
@@ -10,7 +10,9 @@
         <div class=" card-body">
           <p>{{ title }}</p>
           <p>{{ originaltitle }}</p>
-          <p>{{ language }}</p>
+          <div class="flag">
+            <img :src="'/public/img/' + flag + '.png'" :alt="language">
+          </div>
           <p>{{ vote }}</p>
         </div>
 
@@ -26,6 +28,7 @@
 <script>
 import { VueFlip } from 'vue-flip';
 import { store } from '../data/store';
+import {flags} from '../data/data';
 
 export default {
   name: 'FilmListComponent',
@@ -35,18 +38,34 @@ export default {
   data() {
     return {
       store,
+      flagList: flags,
     }
   },
 
   components: {
     'vue-flip': VueFlip
+  },
+
+  computed: {
+    flag() {
+      if (this.flagList.includes(this.language)) {
+        return this.language;
+      } else {
+        return 'unknown';
+      }
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.vue-flip{
+  cursor: pointer;
+}
+
 .card-body{
   background-color: black;
   height: 100%;
+  text-align: center;
 }
 </style>
---
+++ src/data/data.js
@@ -0,0 +1,5 @@
+const flags = ['ar', 'bs', 'cn', 'de', 'en', 'fr', 'hi', 'in', 'it', 'ja', 'ko', 'nl', 'pt', 'ru', 'sp',
+                'sv', 'te', 'th', 'tr', 'us', 'zh',];
+
+
+export {flags};
